af8ccde602142a047ad6d70ea6a898c18e0df543
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-12-04 07:19:38 +0100

    docs(AllIcons): Add usage example and improve layout for icon display

--- src/stories/AllIcons.stories.svelte
+++ src/stories/AllIcons.stories.svelte
@@ -6,6 +6,7 @@
   import { VANILLA_THEME_CLASS_MAP } from "../../.storybook/components/stories/constants.js";
   import metadata from "../../metadata.json" with { type: "json" };
   import * as icons from "../lib/icons/index.js";
+  import { IconsOptimizationProvider } from "../lib/index.js";
 
   const { Story } = defineMeta({
     tags: ["!autodocs"],
@@ -17,6 +18,7 @@
   let pascalCaseIconName = $derived(
     selectedIcon ? pascalToKebabCase(selectedIcon) : "",
   );
+  let searchElement = $state<HTMLInputElement>();
   let selectedIconMetadata = $derived(
     metadata[
       (pascalCaseIconName
@@ -71,6 +73,12 @@
     }, 2000);
   }
 
+  function handleTagSelect(tag: string) {
+    search = tag;
+    selectedIcon = null;
+    searchElement?.focus();
+  }
+
   // filtering
   const iconsWithMetadata = (Object.keys(icons) as (keyof typeof icons)[]).map(
     (iconName) => {
@@ -109,198 +117,226 @@
 </script>
 
 <Story name="All Icons" asChild tags={["!dev"]}>
-  <div class={["icons-explorer", vanillaThemeClass]}>
-    <form class="p-search-box" onsubmit={handleSubmit}>
-      <label class="u-off-screen" for="search">Search</label>
-      <!-- svelte-ignore a11y_autofocus -->
-      <input
-        type="search"
-        id="search"
-        class="p-search-box__input"
-        name="search"
-        placeholder={`Search ${Object.keys(icons).length} icons...`}
-        required
-        autocomplete="on"
-        autofocus
-        bind:value={search}
-      />
-      <button type="reset" class="p-search-box__reset"
-        ><icons.CloseIcon />
-      </button>
-      <button type="submit" class="p-search-box__button"
-        ><icons.SearchIcon /></button
-      >
-    </form>
-    <div class="icons-grid">
-      {#each filteredIcons as icon (icon)}
-        <button
-          class={[
-            "icon-button",
-            "p-button--base",
-            "is-dense",
-            "is-small",
-            "p-tooltip p-tooltip--btm-center",
-            vanillaThemeClass,
-          ]}
-          onclick={(event) => handleIconSelect(event, icon.name)}
-        >
-          <icon.Component />
-          <span class="p-tooltip__message" role="tooltip"
-            >{pascalToKebabCase(icon.name)}</span
-          >
+  <IconsOptimizationProvider>
+    <div class={["icons-explorer", vanillaThemeClass]}>
+      <form class="p-search-box" onsubmit={handleSubmit}>
+        <label class="u-off-screen" for="search">Search</label>
+        <!-- svelte-ignore a11y_autofocus -->
+        <input
+          type="search"
+          id="search"
+          class="p-search-box__input"
+          name="search"
+          placeholder={`Search ${Object.keys(icons).length} icons...`}
+          required
+          autocomplete="on"
+          autofocus
+          bind:value={search}
+          bind:this={searchElement}
+        />
+        <button type="reset" class="p-search-box__reset"
+          ><icons.CloseIcon />
         </button>
-      {/each}
-    </div>
-  </div>
-
-  <!-- svelte-ignore a11y_no_static_element_interactions -->
-  {#if selectedIcon && SelectedIconComponent}
-    <!-- svelte-ignore a11y_click_events_have_key_events -->
-    <div
-      class="p-modal"
-      onclick={(e) => {
-        e.target === e.currentTarget && (selectedIcon = null);
-      }}
-    >
-      <!-- svelte-ignore a11y_no_noninteractive_element_to_interactive_role -->
-      <section
-        class="p-modal__dialog"
-        role="dialog"
-        aria-modal="true"
-        aria-labelledby="Use Icon"
-      >
-        <header class="p-modal__header">
-          <h2 class="p-modal__title">
-            Use Icon <code>{pascalCaseIconName}</code>
-          </h2>
+        <button type="submit" class="p-search-box__button"
+          ><icons.SearchIcon /></button
+        >
+      </form>
+      <div class="icons-grid">
+        {#each filteredIcons as icon (icon)}
           <button
-            class="p-modal__close"
-            aria-label="Close active modal"
-            aria-controls="modal"
-            onclick={() => (selectedIcon = null)}>Close</button
+            class={[
+              "icon-button",
+              "p-button--base",
+              "is-dense",
+              "is-small",
+              "p-tooltip p-tooltip--btm-center",
+              vanillaThemeClass,
+            ]}
+            onclick={(event) => handleIconSelect(event, icon.name)}
           >
-        </header>
-        <div class="modal-content">
-          <div class="icon-preview">
-            <div class="grid-background"></div>
-            <SelectedIconComponent />
-          </div>
-          {#if selectedIconMetadata}
-            <div class="icon-tags">
-              {#each selectedIconMetadata.tags as tag}
-                <span class="p-chip is-readonly is-dense">
-                  <span class="p-chip__value">{tag}</span>
-                </span>
-              {/each}
-            </div>
-          {/if}
-        </div>
-
-        <footer class="p-modal__footer">
-          <button
-            class="u-no-margin--bottom"
-            aria-controls="modal"
-            onclick={() =>
-              handleCopyToClipboard(selectedIcon as keyof typeof icons)}
-            disabled={copiedToClipboard}
-          >
-            {#if copiedToClipboard}
-              Copied to clipboard
-            {:else}
-              Copy
-            {/if}
+            <icon.Component />
+            <span class="p-tooltip__message" role="tooltip"
+              >{pascalToKebabCase(icon.name)}</span
+            >
           </button>
-        </footer>
-      </section>
+        {/each}
+      </div>
     </div>
-  {/if}
 
-  <style>
-    :root {
-      --icon-button-size: 1.5rem;
-      --icon-button-padding: 1rem;
-      --icon-preview-size: 13rem;
-      --icon-preview-padding: 2rem;
-    }
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    {#if selectedIcon && SelectedIconComponent}
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <div
+        class="p-modal"
+        onclick={(e) => {
+          e.target === e.currentTarget && (selectedIcon = null);
+        }}
+      >
+        <!-- svelte-ignore a11y_no_noninteractive_element_to_interactive_role -->
+        <section
+          class="p-modal__dialog"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="Use Icon"
+        >
+          <header class="p-modal__header">
+            <h2 class="p-modal__title">Icon Details</h2>
+            <button
+              class="p-modal__close"
+              aria-label="Close active modal"
+              aria-controls="modal"
+              onclick={() => (selectedIcon = null)}>Close</button
+            >
+          </header>
+          <div class="modal-content">
+            <div class="icon-preview">
+              <div class="grid-background"></div>
+              <SelectedIconComponent />
+            </div>
+            <div class="icon-info">
+              <div>
+                <h3>
+                  {pascalCaseIconName}
+                </h3>
+                <span>
+                  <code>
+                    {`<${selectedIcon} />`}
+                  </code>
+                </span>
+              </div>
+              {#if selectedIconMetadata}
+                <div class="icon-tags">
+                  {#each selectedIconMetadata.tags as tag}
+                    <button
+                      class="p-chip is-dense"
+                      onclick={() => handleTagSelect(tag)}
+                    >
+                      <span class="p-chip__value">{tag}</span>
+                    </button>
+                  {/each}
+                </div>
+              {/if}
+            </div>
+          </div>
 
-    .icons-explorer {
-      min-height: 32rem;
+          <footer class="p-modal__footer">
+            <button
+              class="u-no-margin--bottom"
+              aria-controls="modal"
+              onclick={() =>
+                handleCopyToClipboard(selectedIcon as keyof typeof icons)}
+              disabled={copiedToClipboard}
+            >
+              {#if copiedToClipboard}
+                Copied to clipboard
+              {:else}
+                Copy
+              {/if}
+            </button>
+          </footer>
+        </section>
+      </div>
+    {/if}
 
-      .icons-grid {
-        margin: 1rem 0.5rem;
-        display: grid;
-        grid-template-columns: repeat(
-          auto-fill,
-          minmax(
-            calc(var(--icon-button-size) + var(--icon-button-padding) * 2),
-            1fr
-          )
-        );
-        gap: 1em;
+    <style>
+      :root {
+        --icon-button-size: 1.5rem;
+        --icon-button-padding: 1rem;
+        --icon-preview-size: 13rem;
+        --icon-preview-padding: 2rem;
       }
 
-      .icon-button {
-        display: inline-flex;
-        align-items: center;
-        font-size: var(--icon-button-size);
-        padding: var(--icon-button-padding);
-        aspect-ratio: 1/1;
+      .icons-explorer {
+        min-height: 32rem;
 
-        /* vanilla framework button reset */
-        margin: 0;
+        .icons-grid {
+          /* SB is lagging when there is no inline margin */
+          margin: 1rem 1px 0;
+          /* extra space for tooltip of last row, otherwise overflow is triggered due to SB dynamic canvas height */
+          padding: 0 0 3rem;
+          display: grid;
+          grid-template-columns: repeat(
+            auto-fill,
+            minmax(
+              calc(var(--icon-button-size) + var(--icon-button-padding) * 2),
+              1fr
+            )
+          );
+          gap: 1em;
+        }
+
+        .icon-button {
+          display: inline-flex;
+          align-items: center;
+          font-size: var(--icon-button-size);
+          padding: var(--icon-button-padding);
+          aspect-ratio: 1/1;
+
+          /* vanilla framework button reset */
+          margin: 0;
+        }
       }
-    }
-    .p-modal {
-      .modal-content {
-        margin: 1rem;
-        display: flex;
-        flex-direction: row;
-        gap: 1rem;
-        align-items: end;
-        width: 41rem;
-
-        .icon-preview {
+      .p-modal {
+        .modal-content {
           align-items: center;
           display: flex;
-          padding: var(--icon-preview-padding);
-          font-size: var(--icon-preview-size);
-          min-width: var(--icon-preview-size);
-          min-height: var(--icon-preview-size);
-          aspect-ratio: 1/1;
-          isolation: isolate;
-          justify-content: center;
-          overflow: hidden;
-          position: relative;
+          flex-direction: row;
+          gap: 1rem;
+          margin: 1rem;
+          width: 41rem;
 
-          .grid-background {
-            background-image: linear-gradient(
-                var(--vf-color-border-low-contrast) 1px,
-                transparent 1px
-              ),
-              linear-gradient(
-                90deg,
-                var(--vf-color-border-low-contrast) 1px,
-                transparent 1px
-              );
-            background-size: 0.79rem 0.79rem;
-            position: absolute;
-            inset: -1px;
+          .icon-preview {
+            align-items: center;
+            display: flex;
+            padding: var(--icon-preview-padding);
+            font-size: var(--icon-preview-size);
+            min-width: var(--icon-preview-size);
+            min-height: var(--icon-preview-size);
+            aspect-ratio: 1/1;
+            isolation: isolate;
+            justify-content: center;
+            overflow: hidden;
+            position: relative;
 
-            z-index: -1;
+            .grid-background {
+              background-image: linear-gradient(
+                  var(--vf-color-border-low-contrast) 1px,
+                  transparent 1px
+                ),
+                linear-gradient(
+                  90deg,
+                  var(--vf-color-border-low-contrast) 1px,
+                  transparent 1px
+                );
+              background-size: 0.79rem 0.79rem;
+              position: absolute;
+              inset: -5px;
+
+              z-index: -1;
+            }
           }
-        }
+          .icon-info {
+            align-self: stretch;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin: 1rem 0;
 
-        .icon-tags {
-          display: inline-flex;
-          flex-wrap: wrap;
-          gap: 0.15rem;
+            .icon-tags {
+              display: inline-flex;
+              flex-wrap: wrap;
+              gap: 0.2rem;
+              max-height: 4rem;
+              overflow-y: auto;
 
-          .p-chip {
-            /* vanilla framework chip reset */
-            margin: 0;
+              .p-chip {
+                /* vanilla framework chip reset */
+                margin: 0;
+              }
+            }
           }
         }
       }
-    }
-  </style>
+    </style>
+  </IconsOptimizationProvider>
 </Story>
